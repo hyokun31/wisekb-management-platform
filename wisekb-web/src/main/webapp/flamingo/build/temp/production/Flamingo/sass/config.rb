fac9d4eb85d956742be9e9d6e13449c7f80110be
Park <Park@Parkui-MacBook-Pro.local>
2017-01-31 08:47:09 +0900

    ExtJS 라이브러리 누락파일 추가

--- wisekb-web/src/main/webapp/flamingo/build/temp/production/Flamingo/sass/config.rb
+++ wisekb-web/src/main/webapp/flamingo/build/temp/production/Flamingo/sass/config.rb
@@ -1,2 +1,2 @@
 Compass.add_project_configuration('../../../../../sass/config.rb')
-cache_path = '/Users/user/projects/flamingo-master/flamingo-web/src/main/webapp/flamingo/build/.sass-cache'
+cache_path = '/Users/Park/App/git/wisekb-management-platform/wisekb-web/src/main/webapp/flamingo/build/.sass-cache'
